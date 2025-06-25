5b7cc166170225a405ed235bd37874dfa43b763c
Tea <cha9n.1488@gmail.com>
2025-06-25 11:42:47 +0300

    Done2

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,29 +1,29 @@
 package mate.academy;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-
 import mate.academy.lib.Injector;
-import mate.academy.model.*;
-import mate.academy.service.*;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.service.UserService;
 
 public class Main {
     public static void main(String[] args) {
         Injector injector = Injector.getInstance("mate.academy");
 
         OrderService orderService = (OrderService) injector.getInstance(OrderService.class);
-        ShoppingCartService shoppingCartService = (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+        ShoppingCartService shoppingCartService =
+                (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
         UserService userService = (UserService) injector.getInstance(UserService.class);
 
         User user = userService.findByEmail("test@example.com").get();
         ShoppingCart shoppingCart = shoppingCartService.getByUser(user);
 
-        // Завершити замовлення
         Order order = orderService.completeOrder(shoppingCart);
         System.out.println("Order created: " + order);
 
-        // Отримати історію замовлень
         List<Order> history = orderService.getOrdersHistory(user);
         history.forEach(System.out::println);
     }
